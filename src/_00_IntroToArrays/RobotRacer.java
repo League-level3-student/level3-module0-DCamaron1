e01b3bb7f80b4ce56a8791c064da03ebac2ccf07
DCamaron1 <league@WorthyMini-2.local>
2018-11-24 10:49:11 -0800

    fixed errors

--- src/_00_IntroToArrays/RobotRacer.java
+++ src/_00_IntroToArrays/RobotRacer.java
@@ -2,7 +2,6 @@ package _00_IntroToArrays;
 
 import java.util.Random;
 
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -12,23 +11,28 @@ public class RobotRacer {
 	public static void main(String[] args) {
 		String answer = JOptionPane.showInputDialog("How many robots do you want to race? Your max is 15");
 		int answerNum = Integer.parseInt(answer);
-		Random random = new Random();
-		Robot[] robots = new Robot[answerNum];
-		int x = 30;
-		int y = 500;
-		int spacer = 900/answerNum;
-		for (int i = 0; i < robots.length; i++) {
-			robots[i] = new Robot();
-			robots[i].setSpeed(10);
-			robots[i].setPenColor(random.nextInt(255),random.nextInt(255),random.nextInt(255));
-			robots[i].penDown();
-			robots[i].moveTo(x + spacer*i, y);
-
-		}
-		while (!checkForWinner(robots)) {
+		if (answerNum <= 15) {
+			Random random = new Random();
+			Robot[] robots = new Robot[answerNum];
+			int x = 30;
+			int y = 500;
+			int width = 900;
+			int spacer = width / answerNum;
 			for (int i = 0; i < robots.length; i++) {
-				robots[i].move(random.nextInt(101));
+				robots[i] = new Robot();
+				robots[i].setSpeed(10);
+				robots[i].setPenColor(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+				robots[i].penDown();
+				robots[i].moveTo(x + spacer * i, y);
 			}
+
+			while (!checkForWinner(robots)) {
+				for (int i = 0; i < robots.length; i++) {
+					robots[i].move(random.nextInt(101));
+				}
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Im sorry, try again.");
 		}
 	}
 
